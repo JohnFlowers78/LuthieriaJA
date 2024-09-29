e3889a6e4f0b63d070f8dfe3cbcb31ddd692ec39
AlissonPF <alissonpatrickfarias@gmail.com>
2024-09-28 21:56:25 -0300

    End Points de cliente feitos e outras correções

--- OficinaLuthier/API/Models/Cliente.cs
+++ OficinaLuthier/API/Models/Cliente.cs
@@ -12,5 +12,6 @@ public class Cliente
   public Guid Id { get; set;}
   public string? Nome { get; set; }
   public string? Telefone { get; set; }
+  public string Cpf {get; set; }
   public DateTime CriadoEm { get; set; }
 }
--- OficinaLuthier/API/Program.cs
+++ OficinaLuthier/API/Program.cs
@@ -6,45 +6,80 @@ var app = builder.Build();
 
 
 List<Cliente> clientes = [
-    new Cliente { Nome = "Luis", Telefone = "41984707329" },
-    new Cliente { Nome = "Victor", Telefone = "41997438180" },
-    new Cliente { Nome = "Carmem", Telefone = "41963457075" },
-    new Cliente { Nome = "Otavio", Telefone = "41988623514" },
-    new Cliente { Nome = "João Pedro", Telefone = "41999125678" },
-    new Cliente { Nome = "Óculos de Sol", Telefone = "43990842030"},
-    new Cliente { Nome = "Mochila", Telefone = "47988702098" },
-    new Cliente { Nome = "Relógio", Telefone = "42988635587" },
-    new Cliente { Nome = "Camisa Social", Telefone = "41995234578" },
-    new Cliente { Nome = "Tênis Casual", Telefone = "45987889339" }
+    new Cliente { Id = Guid.Parse("1"), Nome = "Luis", Telefone = "41984707329" },
+    new Cliente { Id = Guid.Parse("2"), Nome = "Victor", Telefone = "41997438180" },
+    new Cliente { Id = Guid.Parse("3"), Nome = "Carmem", Telefone = "41963457075" },
+    new Cliente { Id = Guid.Parse("4"), Nome = "Otavio", Telefone = "41988623514" },
+    new Cliente { Id = Guid.Parse("5"), Nome = "João Pedro", Telefone = "41999125678" },
+    new Cliente { Id = Guid.Parse("6"), Nome = "Luisa", Telefone = "43990842030"},
+    new Cliente { Id = Guid.Parse("7"), Nome = "Vitória", Telefone = "47988702098" },
+    new Cliente { Id = Guid.Parse("8"), Nome = "Julia", Telefone = "42988635587" },
+    new Cliente { Id = Guid.Parse("9"), Nome = "Marcela", Telefone = "41995234578" },
+    new Cliente { Id = Guid.Parse("10"), Nome = "Marcia", Telefone = "45987889339" }
 ];
 List<Funcionario> funcionarios = 
 [
-    new Funcionario{Nome = "Cleiton", Cargo = "Luthier"},
-    new Funcionario{Nome = "Antônio", Cargo = "Luthier Aprendiz"}
+    new Funcionario{ Id = Guid.Parse("1"), Nome = "Cleiton", Cargo = "Luthier"},
+    new Funcionario{ Id = Guid.Parse("2"), Nome = "Antônio", Cargo = "Luthier Aprendiz"}
 ];
 List<OrdemDeServico> ordensServicos = 
 [
-    new OrdemDeServico{DescricaoServico = "Lustragem", FuncionarioId = null, Instrumento = "Trombone", Status = "Pendente", ValorEstimado = 130.00},
-    new OrdemDeServico{DescricaoServico = "Revisão", FuncionarioId = null, Instrumento = "Fagote", Status = "Pendente", ValorEstimado = 200.00}
+    new OrdemDeServico{ Id = Guid.Parse("1"), DescricaoServico = "Lustragem", FuncionarioId = Guid.Parse("1"), Instrumento = "Trombone", Status = "Pendente", ClienteId = Guid.Parse("1"), ValorEstimado = 130.00},
+    new OrdemDeServico{ Id = Guid.Parse("2"), DescricaoServico = "Revisão", FuncionarioId = Guid.Parse("2"), Instrumento = "Fagote", Status = "Pendente", ClienteId = Guid.Parse("3"), ValorEstimado = 200.00}
 ];
 
-
+// ## Listar
 app.MapGet("/cliente", () =>
 {
+    if(clientes.Count == 0){
+        return Results.NotFound();
+    }
     return Results.Ok(clientes.ToList());
 });
-app.MapPost("", () => {
 
+//## Cadastrar
+app.MapPost("/cliente", ([FromBody] Cliente cliente) => {
+    Cliente clienteTemp = cliente;
+    var confere = clientes.FirstOrDefault(x => x.Cpf == clienteTemp.Cpf);
+    if (confere == null){
+        clientes.Add(clienteTemp);
+        return Results.Ok(clienteTemp);
+    }
+    return Results.BadRequest("Cliente já cadastrado");
 });
-app.MapPost("", () => {
 
+//## Buscar 
+app.MapGet("cliente/{cpf}", ([FromRoute] string cpf) => {
+    Cliente cliente = clientes.FirstOrDefault(x => x.Cpf == cpf);
+    if (cliente == null){
+        return Results.NotFound("Cliente não encontrado");
+    }
+    return Results.Ok(cliente);
 });
-app.MapPost("", () => {
 
-});
-app.MapPost("", () => {
 
+//## Alterar
+app.MapPut("cliente/{cpf}", ([FromBody] Cliente cliente) => {
+    Cliente clienteTemp = clientes.FirstOrDefault(x => x.Cpf == cliente.Cpf);
+    if (clienteTemp == null){
+        return Results.NotFound();
+    }
+    clienteTemp.Nome = cliente.Nome;
+    clienteTemp.Telefone = cliente.Telefone;
+    clientes.Add(clienteTemp);
+    return Results.Ok(clienteTemp);
 });
+
+//## Deletar
+app.MapDelete("cliente/cpf", ([FromRoute] string cpf) => {
+    Cliente cliente = clientes.FirstOrDefault(x => x.Cpf == cpf);
+    if (cliente == null){
+        return Results.NotFound("Cliente não encontrado");
+    }
+    clientes.Remove(cliente);
+    return Results.Ok(cliente);
+});
+
 app.MapPost("", () => {
 
 });
